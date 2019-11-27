dc0af5a2b5320c2c7025c463170de3599436485f
Oluseyi Olaleye <ictoluseyi@gmail.com>
2019-11-27 20:44:20 +0100

    Initial commit

---
+++ public_html/js/script.js
@@ -0,0 +1,29 @@
+
+$('.show').click(function() {
+	// $('#show').text('');
+	$('#show').css('display','none')
+	$('.view').css('display', 'inline');
+});
+$('.show2').click(function() {
+	$('#show2').css('display','none')
+	$('.view2').css('display', 'inline');
+});
+$('.show3').click(function() {
+	$('#show3').css('display','none')
+	$('.view3').css('display', 'inline');
+});
+$('.show4').click(function() {
+	$('#show4').css('display','none')
+	$('.view4').css('display', 'inline');
+});
+$('.show5').click(function() {
+	$('#show5').css('display','none')
+	$('.view5').css('display', 'inline');
+});
+$('.show6').click(function() {
+	$('#show6').css('display','none')
+	$('.view6').css('display', 'inline');
+});
+
+
+
